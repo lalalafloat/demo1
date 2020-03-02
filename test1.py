2722b740b355a40e23bd4c357e129aac7249e22c
lalalafloat <1562713306@qq.com>
2020-03-02 20:38:39 +0800

    **

--- test1.py
+++ test1.py
@@ -1,7 +1,17 @@
+import logging
+import sys
 def sorted(a):
+    logging.info("a = {}".format(a))
     a.sort()    
     return a
 
 if __name__ == '__main__':
     a = [1,5,2,3]
     print sorted(a)
+    x = []
+    print sys.getrefcount(x)
+    y = x
+    z = y
+    print sys.getrefcount(x)
+    x = 1 
+    print sys.getrefcount(x)

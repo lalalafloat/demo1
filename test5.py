aea62a459d743a10d2ec59ac3a8d4bfadc1e5858
lalalafloat <1562713306@qq.com>
2020-03-04 15:36:28 +0800

    update test5.py  加入了回溯

--- test5.py
+++ test5.py
@@ -3,11 +3,12 @@ import random
 import copy
 
 class edge(object):
-    def __init__(self, from_id, to_id, next_edge_id,  weight = 1):
+    def __init__(self, from_id, to_id, next_edge_id, is_required, weight = 1):
         self.from_id = from_id
         self.to_id = to_id
         self.weight = weight
         self.nxt = next_edge_id
+        self.is_required = is_required
 
 class node(object):
     def __init__(self, node_id, repeat_count = 1):
@@ -26,12 +27,12 @@ class connect_map(object):
             self.nod.append(node(0,i))
             self.head.append(0)
 
-    def addedge(self, from_id, to_id, weight = 1.0):
+    def addedge(self, from_id, to_id, is_required, weight = 1.0):
         self.tot += 1
-        self.ed.append(edge(from_id, to_id, self.head[from_id], weight))
+        self.ed.append(edge(from_id, to_id, self.head[from_id], is_required, weight))
         self.head[from_id] = self.tot
 
-def generate_idxs(cm, node_id, lst):
+def generate_idxs(cm, node_id, lst, end):
     #print lst
     edge_id = cm.head[node_id]
     candicates = {}
@@ -41,20 +42,26 @@ def generate_idxs(cm, node_id, lst):
             candicates[cm.ed[edge_id].to_id] = cm.ed[edge_id].weight
             rate_sum += cm.ed[edge_id].weight
         edge_id = cm.ed[edge_id].nxt
-    random_score = random.uniform(0,1.0)
-    candicate = None
-    for k,v in candicates.items():
-        random_score -= 1.0*v/rate_sum
-        if random_score <= 0:
-            candicate = k
-            break
-    if candicate != None:
-        lst.append(candicate)
-        cm.nod[candicate].repeat_count -= 1
-        generate_idxs(cm, candicate, lst)
-        return lst
-    else:
-        return lst
+    
+    while True:
+        random_score = random.uniform(0,rate_sum)
+        candicate = None
+        for k,v in candicates.items():
+            random_score -= v
+            if random_score <= 0:
+                candicate = k
+                break
+        if candicate != None:
+            lst.append(candicate)
+            cm.nod[candicate].repeat_count -= 1
+            lst = generate_idxs(cm, candicate, lst, end)
+            if lst[-1] != end:
+                lst.pop()
+                cm.nod[candicate].repeat_count += 1
+            else:
+                return lst
+        else:
+            return lst
 
 def dfs(cm, node_id, lst):
     #print node_id
@@ -70,20 +77,21 @@ if __name__ == '__main__':
     cm.init_node(10)
     cm.nod[3].repeat_count = 3
     cm.nod[6].repeat_count = 2
-    cm.addedge(0,1)
-    cm.addedge(1,2)
-    cm.addedge(2,3)
-    cm.addedge(3,3, 0.3)
-    cm.addedge(3,6,0.3)
-    cm.addedge(6,3)
-    cm.addedge(3,4,0.4)
-    cm.addedge(4,5)
+    cm.addedge(0,1,1)
+    cm.addedge(1,2,1)
+    cm.addedge(2,3,1)
+    cm.addedge(3,3,1,0.3)
+    cm.addedge(3,6,1,0.3)
+    cm.addedge(6,3,1)
+    cm.addedge(3,4,1,0.4)
+    cm.addedge(4,5,1)
     bad_case = 0
     all_case = 0
     for i in range(100):
         am = copy.deepcopy(cm)
-        lst = generate_idxs(am, 0, [])
+        lst = generate_idxs(am, 0, [], 5)
         all_case += 1
+        print "{} : {}".format(all_case, lst)
         if lst[len(lst)-1] != 5:
             bad_case += 1
             print "{} : {}".format(bad_case, lst)

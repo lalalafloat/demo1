088d526841e3d296718a5bd7f0839888f537aa8f
lalalafloat <1562713306@qq.com>
2020-03-30 13:43:49 +0800

    update

--- re模块.py
+++ re模块.py
@@ -2,13 +2,14 @@
 import re
 
 if __name__ == '__main__':
-    a = u"首付3000元起"
+    a = u"首付3000起"
     b = re.search('\d+', a).group()
     print b
     c = [u'元', u'利息', u'息']
     for i in c:
-        b = re.search(i, a).group()
+        b = re.search(i, a)
         print "*"
         if b:
+            b = b.group()
             break
     print b

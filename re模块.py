a01ee6e7c7a35c5995c6f428ca17a086d8eca234
lalalafloat <1562713306@qq.com>
2020-03-31 21:33:26 +0800

    update newhead

--- re模块.py
+++ re模块.py
@@ -2,9 +2,9 @@
 import re
 
 if __name__ == '__main__':
-    a = u"首付3000起"
-    b = re.search('\d+', a).group()
-    print b
+    a = "奥迪A4首付3000元起"
+    b = re.findall('\d+', a)
+    print b[-1]
     c = [u'元', u'利息', u'息']
     for i in c:
         b = re.search(i, a)
